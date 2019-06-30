0bdfbaf9cac714cf137523e9772e309694a76cb7
juju2143 <juju2143@gmail.com>
2019-06-30 16:32:48 -0400

    First commit!

---
+++ README.md
@@ -0,0 +1,35 @@
+# twilight32
+
+Twilight32 is a fun and retro-looking web-based IDE not unlike fantasy consoles such as PICO-8, aimed at retro game enthusiasts who want to develop on a platform similar to old computers from the 80's.
+
+## Features
+
+- Modern code editor with modern features based on [monaco-editor](https://microsoft.github.io/monaco-editor/)
+- [wwwBASIC](https://github.com/google/wwwbasic)
+- Sprite editor
+- Music tracker with MIDI support
+- Standalone app and player
+- Various export options
+- Lots of tools to help you
+
+Note: these features might not be implemented yet until the project hits version 1.0.
+
+## Installing
+
+### Web
+
+Just dump this repo on some web space, don't forget to install external libraries with `npm install`.
+
+### Standalone
+
+#soon
+
+## Credits
+
+Main developer: [@juju2143](https://github.com/juju2143)
+
+Contributors:
+
+The Twilight32 code is licensed under the terms of the Québec Free and Open-Source Licence – Permissive (LiLiQ-P), version 1.1.
+
+If you liked it, you can tip the main developer on [Patreon](https://patreon.com/juju2143)!
---
+++ main.js
@@ -0,0 +1,61 @@
+require.config({ paths: { 'vs': 'node_modules/monaco-editor/min/vs' }});
+require(['vs/editor/editor.main'], main);
+
+var editors = [];
+
+function main() {
+    newEditor(0);
+    newRun(0);
+}
+
+function run(i) {
+    i = parseInt(i, 10);
+    var basic_out = $('#run-'+i+' .basic_out')[0];
+    window.Basic(editors[i].getValue(), basic_out);
+}
+
+function newEditor(i)
+{
+    i = parseInt(i, 10);
+    $('main').append('<section id="editor-'+i+'" checked><div class="editor"></div></section>');
+    //$('#tabs ul').append('<li><a href="#editor-'+i+'">Editor '+i+'</a></li>');
+    $('#tabs').append('<input name="tabs" type="radio" id="tab-editor-'+i+'" class="tab-input"/>');
+    $('#tabs').append('<label for="tab-editor-'+i+'" class="tab-label">Editor</label>');
+    editors[i] = monaco.editor.create($('#editor-'+i+' .editor')[0], {
+        value: [
+            'PRINT "Hello World!"',
+            'FOR i = 1 to 10',
+            '\tPRINT "Counting "; i',
+            'NEXT i',
+        ].join('\n'),
+        language: 'basic',
+        theme: "vs-dark",
+        fontFamily: "VT323",
+        fontSize: 20
+    });
+    $('#tab-editor-'+i).on('change', function(e) {
+        if(this.checked)
+        {
+            $('section').css('display', 'none');
+            $('#editor-'+i).css('display', 'block');
+            editors[i].layout();
+        }
+    });
+}
+
+function newRun(i)
+{
+    i = parseInt(i, 10);
+    $('main').append('<section id="run-'+i+'"><canvas class="basic_out" width="640" height="480"></canvas></section>');
+    //$('#tabs ul').append('<li><a href="#run-'+i+'" onclick="run('+i+')">Run '+i+'</a></li>');
+    $('#tabs').append('<input name="tabs" type="radio" id="tab-run-'+i+'" class="tab-input"/>');
+    $('#tabs').append('<label for="tab-run-'+i+'" class="tab-label">Run</label>');
+    $('#tab-run-'+i).on('change', function(e) {
+        if(this.checked)
+        {
+            $('section').css('display', 'none');
+            $('#run-'+i).css('display', 'block');
+            run(i);
+        }
+    });
+}
